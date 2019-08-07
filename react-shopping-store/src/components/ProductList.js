90327a698eb84cd9a00cb87d2f8957fe24b40d56
murshidmac <mh.murshidco.uk@outlook.com>
2019-08-07 10:12:01 +0530

    data set moving for context api

--- react-shopping-store/src/components/ProductList.js
+++ react-shopping-store/src/components/ProductList.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
 import Product from './Product';
+import Title from './styled/Title';
+import { storeProducts } from '../data';
 export default class ProductList extends Component {
+
+    state = {
+        products: []
+    }
+
+    componentWillMount(){
+        const list = this.axiosGetProducts();
+        console.log("Getting the list ", list)
+        this.setState({products: list})
+    }
+
+    //Do the rest call from here
+    axiosGetProducts(){
+        return storeProducts;
+    }
+
+
     render() {
         return (
-            <div>
-                <Product/>
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="Our" title="Products" />
+                        {/* Product Row */}
+                        < div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+
+            // <div>
+            //     <Product/>
+            // </div>
         )
     }
 }
